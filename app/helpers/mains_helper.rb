fcd64ce3b7c4e00e6bf63a0af761ea0b32a58b00
Villager <elvis4600@gmail.com>
2018-02-12 14:41:13 +0800

    move logic out of view

--- app/helpers/mains_helper.rb
+++ app/helpers/mains_helper.rb
@@ -1,2 +1,20 @@
 module MainsHelper
+  
+  def active_first_item(index)
+    'active' if index ==0
+  end
+
+  def show_instagram_photos(photo)
+    case photo['type']
+      # show video source
+      when "video"
+        render 'partial/video_source', soruce: photo
+      # show mulitiple images source
+      when "carousel"
+        render 'partial/carousel_source', source: photo
+      # show one image source
+      when "image"
+        render 'partial/image_source', source: photo
+    end
+  end
 end
